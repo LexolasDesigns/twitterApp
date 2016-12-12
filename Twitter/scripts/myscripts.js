1ff890b91c73b79791a58d1a2e91b0024f409099
LexolasDesigns <lexolasdesigns@gmail.com>
2016-12-12 10:21:50 -0600

    Add files via upload

---
+++ Twitter/scripts/myscripts.js
@@ -0,0 +1,14 @@
+var app = angular.module("twitterApp", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "partials/main.html"
+    })
+    .when("/celebritySearch", {
+        templateUrl : "partials/celeb.html"
+    })
+    .when("/sportsSearch", {
+        templateUrl : "partials/sports.html"
+    });
+});
