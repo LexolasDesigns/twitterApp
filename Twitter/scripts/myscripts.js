99132aec1d21eb6ce6635e056b6f6aefc808c957
LexolasDesigns <lexolasdesigns@gmail.com>
2016-12-19 08:04:13 -0600

    Add files via upload
    Update, doesn't work yet but it will!

--- Twitter/scripts/myscripts.js
+++ Twitter/scripts/myscripts.js
@@ -1,14 +1,43 @@
-var app = angular.module("twitterApp", ["ngRoute"]);
+var app = angular.module("twitterApp", ["ngRoute","ngTweets"]);
 
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "partials/main.html"
+        templateUrl : "partials/main.html",
+
     })
-    .when("/celebritySearch", {
-        templateUrl : "partials/celeb.html"
+    .when("/jokesSearch", {
+        templateUrl : "partials/jokes.html",
+        controller : "jokesCtrl"
     })
-    .when("/sportsSearch", {
-        templateUrl : "partials/sports.html"
+    .when("/factsSearch", {
+        templateUrl : "partials/facts.html",
+        controller : "factsCtrl"
+    });
+});
+
+app.controller("jokesCtrl", function ($scope, $http, tweets){
+
+    tweets.get({
+        widgetId: '810846962815746049'
+    }).success(function(data){
+        $scope.feed = data;
+        console.log(data);
+    });
+
+    // Load the JSON data with an $HTTP request
+    // $http.get("https://api.twitter.com/1.1/search/tweets.json?q=%40twitterapi")
+    // .then(function(response) {
+    //     $scope.recipeItems = response.data.hits;
+    // });
+});
+
+app.controller("factsCtrl", function ($scope, $http, tweets){
+    // Load the JSON data with an $HTTP request
+        tweets.get({
+        widgetId: '810691191901667330'
+    }).success(function(data){
+        $scope.feed = data;
+        console.log(data);
     });
 });
